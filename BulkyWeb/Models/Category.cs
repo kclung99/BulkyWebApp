8262b1b79c78f3c58c3163d75c50bd6dc43ac3bd
KC Lung <kclung99@gmail.com>
2022-11-29 17:30:02 +0800

    Finish scene change with bootswatch

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,6 +1,14 @@
-﻿namespace BulkyWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
 {
-    public class Class
+    public class Category
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; }
     }
 }
