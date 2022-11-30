045084c6f892df74d770c9e3dd80899b59388c28
KC Lung <kclung99@gmail.com>
2022-11-30 10:12:08 +0800

    Finish create action server side validation

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -18,5 +18,30 @@ namespace BulkyWeb.Controllers
             IEnumerable<Category> categories = _db.Categories.ToList();
             return View(categories);
         }
+
+        // Get
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            // Custom error
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name");
+            }
+            if (ModelState.IsValid) 
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
